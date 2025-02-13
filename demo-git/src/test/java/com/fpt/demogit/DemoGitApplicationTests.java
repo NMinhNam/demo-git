44306c6a8e6ad8a9d204baa734fcb1a69aa121fb
Nguyen Minh Nam <minhnam1810@gmai.com>
2025-02-13 13:00:05 +0700

    config(base): create project

---
+++ demo-git/src/test/java/com/fpt/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
